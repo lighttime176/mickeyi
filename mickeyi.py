97952c5490c1a85ff41848c6b665f309c54014d8
lighttime176 <69344060+lighttime176@users.noreply.github.com>
2024-11-20 16:05:14 +0800

    Update mickeyi.py

--- mickeyi.py
+++ mickeyi.py
@@ -25,7 +25,7 @@ data = {
     'invite_code': '3guPMWLn',
     'email_code': '',
 }
-v2_url = ['0','1','2','3','4','5','6','7','8','9']
+v2_url = ['0','1']
 for i in range(2):
         account = ''
         randomlength = 10
