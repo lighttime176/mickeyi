35de44334a6bf88d07159f32755f9e5d989e5c91
lighttime176 <69344060+lighttime176@users.noreply.github.com>
2024-11-23 15:24:21 +0800

    Update mickeyi.py

--- mickeyi.py
+++ mickeyi.py
@@ -1,70 +1,69 @@
-import requests
-import random
-import time
-headers = {
-        'accept': 'application/json, text/plain, */*',
-        'accept-language': 'zh-CN,zh;q=0.9',
-        'content-language': 'zh-CN',
-        # 'cookie': 'p_uv_id=242167a500065d17dcfba99b8a3e4d7c',
-        'origin': 'https://business.mickey.business',
-        'priority': 'u=1, i',
-        'referer': 'https://business.mickey.business/',
-        'sec-ch-ua': '"Chromium";v="130", "Google Chrome";v="130", "Not?A_Brand";v="99"',
-        'sec-ch-ua-mobile': '?0',
-        'sec-ch-ua-platform': '"Windows"',
-        'sec-fetch-dest': 'empty',
-        'sec-fetch-mode': 'cors',
-        'sec-fetch-site': 'same-origin',
-        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36',
-}
+import random,time
+import imaplib
+import email
+import re
+import email5
+from DrissionPage import ChromiumPage
+from DrissionPage import ChromiumOptions
+
+# 创建页面对象
+co = ChromiumOptions().auto_port()  # 指定程序每次使用空闲的端口和临时用户文件夹创建浏览器
+co.headless(True)   # 无头模式
+co.set_argument('--no-sandbox')  # 无沙盒模式
+co.set_argument('--headless=new')  # 无界面系统添加
+co.set_paths(browser_path="/opt/google/chrome/google-chrome")  # 设置浏览器路径
+co.set_argument('--disable-gpu')    # 禁用gpu，提高加载速度
+co.set_user_agent("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:65.0) Gecko/20100101 Firefox/65.0") 
+browser = ChromiumPage(co)
+tab = browser.latest_tab
+url = "https://business.mickey.business/#/register?code=VVIJFE2q"
+tab.get(url)
+tab.wait(2)
+print(tab.user_agent)
+
+account = ''
+randomlength = 10
+base_str = '0123456789'
+length = len(base_str) - 1
+for i in range(randomlength):
+    account += base_str[random.randint(0, length)]
+email = f"{account}@176468.xyz"
+print(email)
+ele = tab.ele('css=#app > div > div > div > div > div.p-24 > div:nth-child(3) > div > div > div.n-input-wrapper > div > input')
+print(ele)
+ele.clear()
+ele.input(email)
+time.sleep(1)
+ele = tab.ele('css=#app > div > div > div > div > div.p-24 > div:nth-child(4) > div > button > span')
+print(ele)
+ele.click()
+print('等待邮件验证码：')
+for itime in range(20):
+    time.sleep(1)
+    print(itime)
+
+vcode = email5.email_163()
+
+#print(vcode)
+ele = tab.ele('css=#app > div > div > div > div > div.p-24 > div:nth-child(4) > div > div > div.n-input-wrapper > div > input')
+ele.input(vcode)
+ele = tab.ele('css=#app > div > div > div > div > div.p-24 > div:nth-child(5) > div > div.n-input-wrapper > div.n-input__input > input')
+ele.input('12345678')
+ele = tab.ele('css=#app > div > div > div > div > div.p-24 > div:nth-child(6) > div > div.n-input-wrapper > div.n-input__input > input')
+ele.input('12345678')
+ele = tab.ele('css=#app > div > div > div > div > div.p-24 > div:nth-child(9) > button')
+tab.listen.start(targets='/register')  # 开始监听，指定获取包含该文本的数据包
+ele.click()
+res = tab.listen.wait().response
+res = res.body
+print(res)
+token = res['data']['token']
+v2_url = f"https://sub.mickeyi.lol/api/v1/client/subscribe?token={token}"
+print(v2_url)
+tab.get_screenshot(path=r"./test_browser_page.png", full_page=True)
+with open(r"./test_browser.html", "w", encoding="utf-8") as f:
+    f.write(tab.html)
 
 
-data = {
-    'email': '2121432411341@qq.com',
-    'password': '12345678',
-    'invite_code': '3guPMWLn',
-    'email_code': '',
-}
-v2_url = ['0','1']
-for i in range(2):
-        account = ''
-        randomlength = 10
-        base_str = '0123456789'
-        length = len(base_str) - 1
-        for i_random in range(randomlength):
-            account += base_str[random.randint(0, length)]
-        #print(account)
-        data['email'] = f"{account}@qq.com"
-        
-        response = requests.post('https://business.mickey.business/api/v1/passport/auth/register',
-                                 headers=headers, data=data)
-        # response = requests.post('http://business.mickey.business/api/v1/passport/auth/register', headers=headers, cookies = cookies,data=data, proxies=proxies)
-        
-        #print(response.json())
-        
-        try:
-                response = response.json()
-                token = response['data']['token']
-        except:
-                print(response.text)
-        #print(token)
-        #print(f"https://sub.mickeyi.lol/api/v1/client/subscribe?token={token}")
-        v2_url[i] = f"https://sub.mickeyi.lol/api/v1/client/subscribe?token={token}|"
-        #print(v2_url[i])
-        #time.sleep(120)
-strr = ''
-for i in v2_url:
-        strr = strr + str(i)
-#print('strr:')
-print(strr)
-
-
-
-
-
-
-
-
-
-
-
+browser.quit()
+ 
